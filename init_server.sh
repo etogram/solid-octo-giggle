589369464b1f4bf7ee149858416c3a0a4bd96bee
etogram <et0.gr4m@gmail.com>
2020-12-24 21:37:23 +0100

    yop

--- init_server.sh
+++ init_server.sh
@@ -79,4 +79,8 @@ echo "4. install docker-compose if needed"
 } || { # catch
     # save log for exception 
     echo "docker-compose is not installed"    
+    curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+    echo "check docker-compose"
+    docker-compose --version
 }
