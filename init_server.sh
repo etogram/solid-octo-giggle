3fc03d683dd51edb604d48a566983623dbd26e37
etogram <et0.gr4m@gmail.com>
2020-12-24 21:17:47 +0100

    yop

---
+++ init_server.sh
@@ -0,0 +1,70 @@
+echo "###########################"
+echo "###   init server       ###"
+echo "###########################"
+
+W=`whoami` 
+echo "you are running this file as $W"
+if [ $W != "root" ]
+then  
+echo "You re not log as root"
+echo "You must log as root to init server"
+echo "this pgm is being ended..."
+exit
+else
+echo "..."
+fi
+
+echo "Beginning server init"
+
+echo "1. adapt env file to the server"
+echo "node_projet/app/.env"
+echo "check your env file"
+echo "env file content"
+echo ">>>>>>>>>>>>>>>>"
+cat node_project/app/.env
+echo "<<<<<<<<<<<<<<<<"
+echo "if your env file is ok then continue..."
+echo -n "Continue (Y/n)? "
+read answer
+if [ "$answer" != "${answer#[Y]}" ] ;then
+    echo "..."
+else
+    echo "..."
+    echo "exit"
+    exit
+fi
+echo "2. create your https keys"
+echo "check filenames for this server"
+echo "you have to manually change the filenames in the syntax"
+
+FILE=/etc/letsencrypt/live/planetsmining.fr/fullchain.pem
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+fi
+
+FILE=/etc/letsencrypt/live/planetsmining.fr/fullchain.pem
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+fi
+
+echo "do you want to run certbot container to create the keys?"
+echo -n "Continue (Y/n)? "
+read answer
+if [ "$answer" != "${answer#[Y]}" ] ;then
+    echo "running docker container..."
+    sudo docker run -it --rm --name certbot \
+            -v "/etc/letsencrypt/live:/etc/letsencrypt/live"\
+            -v "/etc/letsencrypt:/etc/letsencrypt" \
+            -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+            -p 80:80 \
+            certbot/certbot certonly
+else
+    echo "avoid running container..."
+fi
+
+echo "3. create the nginx conf file"
+
+sudo docker run -it --rm --name python \
+            -v "$PWD:$PWD" \
+            python:3 \
+            python $PWD/config/init/create_nginx_conf.py
